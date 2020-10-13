6285dfff89ef971eba63c8dcd7d097a1c76571f0
dazamar01 <dazamar01@hotmail.com>
2020-10-13 00:25:49 -0500

    Replacing web files created by create-react-app and sommelier web

---
+++ web/src/Components/General/FacebookLoginComponent/FacebookLoginComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./FacebookLoginComponent.css"
+
+export default class FacebookLoginComponent extends React.Component {
+    render() {
+        return (<a href="" className="fb connect">Entrar usando Facebook</a>
+        )
+    }
+}
