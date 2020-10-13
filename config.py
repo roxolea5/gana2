595890c7dd29e63e882b585e4c49849aec544181
dazamar01 <dazamar01@hotmail.com>
2020-10-13 01:13:28 -0500

    Removing env vars

--- config.py
+++ config.py
@@ -8,13 +8,33 @@ if os.path.exists("config.local.json"):
 
 f = open(path, 'r')
 
-host = os.getenv('gana2_host')
-db = os.getenv('gana2_db')
-user = os.getenv('gana2_db_user')
-pwd = os.getenv('gana2_db_pwd')
-
-
 config = json.loads(f.read())
+
+try:
+    host = os.getenv('db_host')
+    if host is None:
+        host = config.get("db_host")
+
+    db = os.getenv('db_name')
+    if db is None:
+        db = config.get("db_name")
+    
+    user = os.getenv('db_user')
+    if user is None:
+        user = config.get("db_user")
+
+    pwd = os.getenv('db_password')
+    if pwd is None:
+        pwd = config.get("db_password")
+except Exception as e:
+    host = "127.0.0.1"
+    db = "sommelier_dev"
+    user = "user"
+    pwd = "pwd"
+    print("***************** ERROR")
+    print("***************** APP WILL NOT RUN WITH THE DESIRED VALUES")
+    print(str(e))
+
 dbConnString = config.get('DBCONN_STRING_DEV')
 dbConnString = dbConnString.replace("{{user}}", user)
 dbConnString = dbConnString.replace("{{pwd}}", pwd)
